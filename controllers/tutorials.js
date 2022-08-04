1330cc6e6033a267593c8f1b5210209102ab1c88
You <joeyk2k@hotmail.com>
2022-08-04 13:57:37 +0100

    post request created

--- controllers/tutorials.js
+++ controllers/tutorials.js
@@ -1,42 +1,7 @@
+import PostTutorials from './../models/postTutorial.js';
+
 // Main home page to retrieve Tutorials
 export const getTutorials = async (req, res) => {
-	res.status(200).json([
-        {
-          id: 1,
-          title: 'Wind Turbine',
-          likes: 200,
-          youtubeUrl:
-          '/images/template.jpg',
-          level: 'Beginner',
-          materials: ['Aluminium, Steel'],
-          instructions:
-            "Step 1: Take an aluminium sheet and measure a box 42cm x 48cm. Draw a line half way up the 48cm length so you have two boxes measuring 42cm x 24cm. Score the outer lines with the Stanley knife and straight edge. You're not trying to cut through the metal, just create a line that can then be popped out later. A good method is to score once lightly, then a second time a bit deeper. Do not score the 24cm halfway line.",
-        },
-        {
-          id: 2,
-          title: 'Solar fan',
-          likes: 215,
-          youtubeUrl:
-          '/images/template.jpg',
-          level: 'Intermediate',
-          materials: ['Aluminium, Steel'],
-          instructions:
-            "Step 1: Take an aluminium sheet and measure a box 42cm x 48cm. Draw a line half way up the 48cm length so you have two boxes measuring 42cm x 24cm. Score the outer lines with the Stanley knife and straight edge. You're not trying to cut through the metal, just create a line that can then be popped out later. A good method is to score once lightly, then a second time a bit deeper. Do not score the 24cm halfway line.",
-        },
-        {
-          id: 3,
-          title: 'Solar Cooker',
-          likes: 3000,
-          youtubeUrl:
-          '/images/template.jpg',
-          level: 'Expert',
-          materials: ['Aluminium, Steel'],
-          instructions:
-            "Step 1: Take an aluminium sheet and measure a box 42cm x 48cm. Draw a line half way up the 48cm length so you have two boxes measuring 42cm x 24cm. Score the outer lines with the Stanley knife and straight edge. You're not trying to cut through the metal, just create a line that can then be popped out later. A good method is to score once lightly, then a second time a bit deeper. Do not score the 24cm halfway line.",
-        },
-      ]);
-
-
 
 };
 // Specific Tutorial Page
@@ -51,7 +16,16 @@ export const getTutorialsBySearch = async (req, res) => {
 
 // Create Tutorial
 export const createTutorial = async (req, res) => {
+const tutorials = req.body;
 
+const newTutorials = new PostTutorials({...tutorials, creator: req.userId, createdAt: new Date().toISOString() })
+
+try {
+  await newTutorials.save();
+  res.status(201).json(newTutorials)
+} catch (error) {
+  res.status(500).json({ message: error.message})
+}
 }
 
 // Update Tutorial (Not in use)
