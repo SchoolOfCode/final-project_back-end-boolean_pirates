184176bdb91634a1fa9d82ca05fccb9b7ffc612f
Joe <joeyk2k@hotmail.com>
2022-08-08 15:02:10 +0100

    fixed materials again

--- controllers/tutorials.js
+++ controllers/tutorials.js
@@ -30,11 +30,11 @@ export const getTutorialsBySearch = async (req, res) => {};
 // Create Tutorial
 export const createTutorial = async (req, res) => {
 	const tutorials = req.body; //Whole
-	console.log(tutorials.materials[0].split(','))
+	console.log(tutorials.materials.split(','))
 	const newTutorials = new tutorialsSchema({
 		...tutorials,
 		createdAt: new Date().toISOString(),
-		materials: tutorials.materials[0].split(',')
+		materials: tutorials.materials.split(',')
 	});
 
 	try {
@@ -56,7 +56,7 @@ export const deleteTutorial = async (req, res) => {
   return res.status(404).send(`No tutorial with id ${id} found`)};
  try {
   await tutorialsSchema.findByIdAndRemove(id);
-  res.status(200).json({message: "Post with ${id} deleted"});
+  res.status(200).json({message: `Post with ${id} deleted`});
 } catch (error) {
   res.status(404).json({ message: error.message });
 }
